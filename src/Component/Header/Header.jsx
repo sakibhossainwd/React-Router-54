d4c1c2eea097b654f9ef30420c3e98c50b9529c1
Sakib Hossain <sakibhossainceo.com>
2023-11-20 22:26:46 +0600

    header menu and outlate added

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './Component/About/About.jsx';
+import Contact from './Component/Contact/Contact.jsx';
+import ErrorPage from "./Component/ErrorPage/ErrorPage.jsx";
+import Header from './Component/Header/Header.jsx';
+import Home from './Component/Home/Home.jsx';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>,
+//     errorElement: <ErrorPage></ErrorPage>
+//   },
+//   {
+//     path: '/about',
+//     element: <About></About>
+//   },
+//   {
+//     path: '/contact',
+//     element: <Contact></Contact>
+//   }
+// ]);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <Header></Header> */}
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
